c8ef223a8eb69162da6707b075fce19247842bb2
prefixsum <prefixsum@outlook.com>
2024-10-12 16:43:04 +0100

    Install Tailwind CSS

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss'],
   eslint: {
     config: {
       stylistic: true,
