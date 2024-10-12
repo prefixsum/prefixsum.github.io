215fddb61065dfc3a19af1b3e748b246ff43111b
prefixsum <prefixsum@outlook.com>
2024-10-12 23:54:16 +0100

    Turn home heading into link

--- frontend/components/HomeHeading.vue
+++ frontend/components/HomeHeading.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="text-slate-50 font-mono">
-    <h1>
-      prefixsum
-    </h1>
+    <NuxtLink
+      to="/"
+      class="hover:underline"
+    >
+      <h1>
+        prefixsum
+      </h1>
+    </NuxtLink>
     <h1 class="text-sm text-slate-200 pt-1">
       is that an edge node or are you just happy to see me
     </h1>
