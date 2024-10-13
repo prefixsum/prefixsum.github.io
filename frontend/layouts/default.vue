2423a811f101048673a8e61fca2a4ce9f808ad23
prefixsum <prefixsum@outlook.com>
2024-10-13 22:07:17 +0100

    Add footer

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -12,7 +12,18 @@
         <div class="px-5">
           <slot />
         </div>
+        <div class="px-5 mt-8">
+          <div class="pt-3 pb-8 border-t border-slate-700">
+            <p class="text-slate-700 text-sm">
+              prefixsum &copy; {{ date }}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const date = new Date().getFullYear()
+</script>
